3e1994c47985837604ba36924345abe61e7804e8
Grzegorz Pieniak <gp79@tutamail.com>
2025-10-03 21:59:59 +0200

    modified:   src/OpenPdf_01.ps1

--- src/OpenPdf_01.ps1
+++ src/OpenPdf_01.ps1
@@ -1,15 +1,23 @@
+# Skrypt: OpenPdf.ps1
 param(
     [string]$PdfPath,
-    [int]$Page = 1
+    [string]$Page = "76"
 )
 
-# Ścieżka do Adobe Acrobat
-$viewer = "C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.exe"
+# Spróbuj przekonwertować na int
+[int]$pageNum = 76
+if ([int]::TryParse($Page, [ref]$pageNum)) {
+    # OK
+} else {
+    Write-Warning "Nie udało się przekonwertować '$Page' na liczbę, używam 1"
+    $pageNum = 15
+}
+
+$acrobat = "C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.exe"
 
 if (-not (Test-Path $PdfPath)) {
     Write-Error "Nie znaleziono pliku PDF: $PdfPath"
     exit 1
 }
 
-# Adobe Acrobat obsługuje parametr /A "page=<nr>"
-Start-Process -FilePath $viewer -ArgumentList "/A `"page=$Page`" `"$PdfPath`""
+Start-Process -FilePath $acrobat -ArgumentList "/A `"page=$pageNum`" `"$PdfPath`""
