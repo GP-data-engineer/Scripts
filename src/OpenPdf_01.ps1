cb8e29687637cf83634f546d6bf952e89ea245e9
Grzegorz Pieniak <gp79@tutamail.com>
2025-10-03 22:59:50 +0200

    modified:   src/OpenPdf_01.ps1

--- src/OpenPdf_01.ps1
+++ src/OpenPdf_01.ps1
@@ -1,6 +1,6 @@
 # Skrypt: OpenPdf_01.ps1
 param(
-    [string]$Pdf_Path,
+    [string]$pdf_path,
     [string]$Strona_PDF = "1"
 )
 
@@ -15,9 +15,9 @@ if ([int]::TryParse($Strona_PDF, [ref]$pageNum)) {
 
 $acrobat = "C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.exe"
 
-if (-not (Test-Path $Pdf_Path)) {
-    Write-Error "Nie znaleziono pliku PDF: $Pdf_Path"
+if (-not (Test-Path $pdf_path)) {
+    Write-Error "Nie znaleziono pliku PDF: $pdf_path"
     exit 1
 }
 
-Start-Process -FilePath $acrobat -ArgumentList "/A `"page=$pageNum`" `"$Pdf_Path`""
+Start-Process -FilePath $acrobat -ArgumentList "/A `"page=$pageNum`" `"$pdf_path`""
